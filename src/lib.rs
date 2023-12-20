3d918795e9b01025bd9ed6e85d38de3cb42096f1
Xinyu Jiang <bernard5@qq.com>
2023-12-20 22:38:38 +0800

    test: add test for structure

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,26 @@ The KvStore store key/value pairs.
 
 use std::collections::HashMap;
 
-/** A KvStore stores key/value pairs in HashMap.
-# Example
-```
-use kvs::KvStore;
-let mut store = KvStore::new();
-store.set("1".to_owned(),"1".to_owned());
-assert_eq!(store.get("1".to_owned()),Some("1".to_owned()));
-store.remove("1".to_owned());
-assert_eq!(store.get("1".to_owned()), None);
-```
-*/
+
+/// A simple key-value store implementation.
+///
+/// This key-value store allows you to store and retrieve string values using string keys.
+/// It supports basic operations such as setting a key-value pair, getting the value of a key,
+/// and removing a key from the store.
+///
+/// # Examples
+///
+/// ```
+/// use blade_turbo::KvStore;
+///
+/// let mut store = KvStore::new();
+///
+/// store.set("name".to_string(), "John".to_string());
+/// assert_eq!(store.get("name".to_string()), Some("John".to_string()));
+///
+/// store.remove("name".to_string());
+/// assert_eq!(store.get("name".to_string()), None);
+/// ```
 pub struct KvStore {
     data: HashMap<String, String>,
 }
@@ -27,25 +36,80 @@ impl Default for KvStore {
 }
 
 impl KvStore {
-    /// generate a new KvStore which can serve reads and writes
+    /// Generate a new `KvStore` instance.
+    ///
+    /// # Returns
+    ///
+    /// A new `KvStore` instance.
     pub fn new() -> KvStore {
         KvStore {
             data: HashMap::new(),
         }
     }
 
-    /// Set the value of a string key to a string
+    /// Set the value of a string key to a string.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to set.
+    /// * `value` - The value to set.
     pub fn set(&mut self, key: String, value: String) {
         self.data.insert(key, value);
     }
 
-    /// Get the string value of a given string key
+    /// Get the string value of a given string key.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to retrieve the value for.
+    ///
+    /// # Returns
+    ///
+    /// The value associated with the given key, if it exists.
     pub fn get(&self, key: String) -> Option<String> {
         self.data.get(&key).cloned()
     }
 
-    /// Remove a given key
+    /// Remove a given key.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to remove.
     pub fn remove(&mut self, key: String) {
         self.data.remove(&key);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_and_get() {
+        let mut store = KvStore::new();
+        store.set("1".to_owned(), "value1".to_owned());
+        store.set("2".to_owned(), "value2".to_owned());
+
+        assert_eq!(store.get("1".to_owned()), Some("value1".to_owned()));
+        assert_eq!(store.get("2".to_owned()), Some("value2".to_owned()));
+    }
+
+    #[test]
+    fn test_get_nonexistent_key() {
+        let store = KvStore::new();
+
+        assert_eq!(store.get("nonexistent".to_owned()), None);
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut store = KvStore::new();
+        store.set("1".to_owned(), "value1".to_owned());
+        store.set("2".to_owned(), "value2".to_owned());
+
+        store.remove("1".to_owned());
+
+        assert_eq!(store.get("1".to_owned()), None);
+        assert_eq!(store.get("2".to_owned()), Some("value2".to_owned()));
     }
 }
