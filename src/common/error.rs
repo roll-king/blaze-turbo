869d4337e848658c7cf06d91089fcad6889a56c8
xyjiang <bernard5@qq.com>
2023-12-31 05:39:02 +0800

    docs: add comment for error

--- src/common/error.rs
+++ src/common/error.rs
@@ -24,13 +24,33 @@ pub enum KVStoreError {
     UnknownCommandType,
 }
 
+/// Implements the conversion from `io::Error` to `KVStoreError`.
 impl From<io::Error> for KVStoreError {
+    /// Converts an `io::Error` into a `KVStoreError`.
+    ///
+    /// # Arguments
+    ///
+    /// * `err` - The `io::Error` to convert.
+    ///
+    /// # Returns
+    ///
+    /// The converted `KVStoreError`.
     fn from(err: io::Error) -> Self {
         KVStoreError::Io(err)
     }
 }
 
+/// Implements the conversion from `serde_json::Error` to `KVStoreError`.
 impl From<serde_json::Error> for KVStoreError {
+    /// Converts a `serde_json::Error` into a `KVStoreError`.
+    ///
+    /// # Arguments
+    ///
+    /// * `err` - The `serde_json::Error` to convert.
+    ///
+    /// # Returns
+    ///
+    /// The converted `KVStoreError`.
     fn from(err: serde_json::Error) -> Self {
         KVStoreError::Serde(err)
     }
