ab199dd9d5771d4efb7b0099f315c48afa54cc87
xyjiang <bernard5@qq.com>
2023-12-31 07:35:09 +0800

    refactor: use c-s structure

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,13 @@ The KvStore store key/value pairs.
 // #![deny(missing_docs)]
 
 mod common;
-pub mod kvs;
+
+pub mod proto;
+pub mod server;
 pub use common::command::Command;
 pub use common::error::{KVStoreError, Result};
 pub use common::kv::KvStore;
+pub use common::kv_engine::KvsEngine;
+pub use common::sled::SledKvsEngine;
+pub use proto::{Request, Response};
+pub use server::{EngineType, KvServer};
