36effafa19e2e8c718dc48c90cc3ca25a473365b
xyjiang <bernard5@qq.com>
2024-01-02 02:35:56 +0800

    docs: add comment for ThreadPool

--- src/thread_pool/mod.rs
+++ src/thread_pool/mod.rs
@@ -13,15 +13,19 @@ pub use shared_queue_thread_pool::SharedQueueThreadPool;
 
 /// A pool which uses multiple threads to execute tasks.
 pub trait ThreadPool {
-    /// Creates a new thread pool, immediately spawning the specified number of threads.
-    /// Returns an error if any thread fails to spawn. All previously-spawned threads are terminated.
+    /// Creates a new thread pool with the specified number of threads.
+    /// The `threads` parameter determines the number of worker threads that will be spawned in the pool.
+    /// If any thread fails to spawn, an error is returned and all previously-spawned threads are terminated.
+    /// The returned `Result` contains the initialized thread pool on success.
     fn new(threads: usize) -> Result<Self>
     where
         Self: Sized;
 
     /// Spawns a function into the thread pool.
-    /// Spawning always succeeds, but if the function panics, the thread pool continues to operate with the same number of threads.
-    /// The thread count is not reduced, and the thread pool is not destroyed, corrupted, or invalidated.
+    /// The function is executed asynchronously by one of the worker threads in the pool.
+    /// Spawning always succeeds, even if the function panics.
+    /// If the function panics, the thread pool continues to operate with the same number of threads.
+    /// The thread count is not reduced, and the thread pool remains valid and functional.
     fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static;
